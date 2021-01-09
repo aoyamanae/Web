f4fee582394d255dbd49bd9dff3ce365563d1b09
aoyamanae <wsqa_ryo203@yahoo.co.jp>
2021-01-09 16:56:16 +0900

    initial commit

--- chap7/customer-input.php
+++ chap7/customer-input.php
@@ -9,30 +9,30 @@
     $address=$_SESSION['customer']['address'];
     $login=$_SESSION['customer']['login'];
     $password=$_SESSION['customer']['password'];
-    $mail=$_SESSION['customer']['mail'];
+    // $mail=$_SESSION['customer']['mail'];
   } 
 ?> 
 <form class="customer" action="customer-output.php" method="post">
   <table>
     <tr>
       <td>お名前</td>
-      <td><input type="text" name="name" value="<?= $name ?>"></td>
+      <td><input type="text" name="name" value="<?=$name?>"></td>
     </tr>
     <tr>
       <td>ご住所</td>
-      <td><input type="text" name="address" value="<?= $address ?>"></td>
+      <td><input type="text" name="address" value="<?=$address?>"></td>
     </tr>
     <tr>
       <td>ログイン名</td>
-      <td><input type="text" name="login" value="<?= $login ?>"></td>
+      <td><input type="text" name="login" value="<?=$login?>"></td>
     </tr>
     <tr>
       <td>パスワード</td>
-      <td><input type="text" name="password" value="<?= $password ?>"></td>
+      <td><input type="text" name="password" value="<?=$password?>"></td>
     </tr>
     <tr>
-      <td>メールアドレス</td>
-      <td><input type="text" name="mail" value="<?= $mail ?>"></td>   <!--追加-->
+      <td>メールアドレス</td>       <!--追加-->
+      <td><input type="text" name="mail" value="<?=$mail?>"></td>   
     </tr>
   </table>
   <input type="submit" value="確定">
